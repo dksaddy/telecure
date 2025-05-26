39a21bf30fb5ec7efc78441643633563032cd44a
dksaddy <saddypro1@gmail.com>
2025-05-27 01:20:20 +0600

    collecting user name, rollbase access appointment, and appointment layout

---
+++ app/appointment/layout.jsx
@@ -0,0 +1,28 @@
+"use client";
+import { useAuth } from "@/app/context.js/AuthContext";
+
+export default function AppointmentLayout({ children }) {
+    const { user } = useAuth();
+
+    if (!user) {
+        return (
+            <div className="flex items-center justify-center h-screen">
+                <h1 className="text-2xl font-bold">Please Sign in to book an appointment</h1>
+            </div>
+        );
+    }
+    if (user.role === "doctor" ) {
+        return (
+            <div className="flex items-center justify-center h-screen">
+                <h1 className="text-2xl font-bold">You are not authorized to book an appointment</h1>
+            </div>
+        );
+    }
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen">
+            <h1 className="text-2xl font-bold mb-4">Book an Appointment</h1>
+            {children}
+        </div>
+    );
+    
+}
