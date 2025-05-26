bcaa4229e82da2ae4b6a94c826f48e80122507c1
nadibahsan10 <mahsan193099@bscse.uiu.ac.bd>
2025-05-26 21:01:54 +0600

    Doctor login

---
+++ app/auth/doctor/layout.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "../login.css";
+const layout = ({ children }) => {
+  return (
+    <div className="h-screen w-screen login-bg grid grid-cols-3">
+      <img
+        src="../logos/default.png"
+        alt=""
+        className="absolute right-10 top-10 h-[40px]"
+      />
+      <div></div>
+      <div></div>
+      {children}
+    </div>
+  );
+};
+
+export default layout;
