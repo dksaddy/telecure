7c80bcce409e792bcbecd03018b22e6142e0a7b1
dksaddy <saddypro1@gmail.com>
2025-07-14 20:54:35 +0600

    Using ZUstand pop up runtime error so we need to make a api call

--- app/appointment/confirm/page.jsx
+++ app/appointment/confirm/page.jsx
@@ -19,7 +19,7 @@ export default function Confirm() {
 
   const handleSubmit = async () => {
     setLoading(true);
-    delete formData.docDetails; // Remove docDetails from formData before sending
+    
     try {
       const data = new FormData();
 
@@ -64,7 +64,7 @@ export default function Confirm() {
     <div className="grid grid-cols-12 gap-1 px-20 py-8 pt-[80px]">
 
       <div className="col-span-12 md:col-span-4 p-2 rounded-lg shadow-md">
-        <DocDetails doctor={formData.docDetails} />
+        {/*<DocDetails />*/}
       </div>
 
       <div className="col-span-12 md:col-span-8 p-8 bg-gray-50 rounded-lg shadow-md">
@@ -73,12 +73,12 @@ export default function Confirm() {
         <div>
           <h2 className="text-xl font-semibold text-purple-700 mb-3">Appointment Details</h2>
           <div className="grid grid-cols-2 gap-4 text-sm text-gray-700">
-            <p><span className="font-medium">Date:</span> {formData.date}</p>
-            <p><span className="font-medium">Time Slot:</span> {formData.interval.start} - {formData.interval.end}</p>
-            <p><span className="font-medium">Available Range:</span> {formData.timeRange}</p>
-            <p><span className="font-medium">Doctor ID:</span> {formData.docId}</p>
-            <p><span className="font-medium">Status:</span> {formData.status}</p>
-            <p><span className="font-medium">Payment:</span> {formData.paymentStatus ? "Paid" : "Pending"}</p>
+            <p><span className="font-medium">Date:</span> {formData?.date}</p>
+            <p><span className="font-medium">Time Slot:</span> {formData?.interval.start} - {formData?.interval.end}</p>
+            <p><span className="font-medium">Available Range:</span> {formData?.timeRange}</p>
+            <p><span className="font-medium">Doctor ID:</span> {formData?.docId}</p>
+            <p><span className="font-medium">Status:</span> {formData?.status}</p>
+            <p><span className="font-medium">Payment:</span> {formData?.paymentStatus ? "Paid" : "Pending"}</p>
           </div>
         </div>
 
@@ -87,18 +87,18 @@ export default function Confirm() {
         <div className="mt-6">
           <h2 className="text-xl font-semibold text-purple-700 mb-3">Patient Details</h2>
           <div className="grid grid-cols-2 gap-4 text-sm text-gray-700">
-            <p><span className="font-medium">Name:</span> {formData.name}</p>
-            <p><span className="font-medium">Phone:</span> {formData.phone}</p>
-            <p><span className="font-medium">Age:</span> {formData.age}</p>
-            <p><span className="font-medium">Gender:</span> {formData.gender}</p>
-            <p><span className="font-medium">Weight:</span> {formData.weight} kg</p>
-            <p><span className="font-medium">Height:</span> {formData.heightFeet} ft {formData.heightInch} in</p>
+            <p><span className="font-medium">Name:</span> {formData?.name}</p>
+            <p><span className="font-medium">Phone:</span> {formData?.phone}</p>
+            <p><span className="font-medium">Age:</span> {formData?.age}</p>
+            <p><span className="font-medium">Gender:</span> {formData?.gender}</p>
+            <p><span className="font-medium">Weight:</span> {formData?.weight} kg</p>
+            <p><span className="font-medium">Height:</span> {formData?.heightFeet} ft {formData?.heightInch} in</p>
           </div>
         </div>
 
         {/* 3. Medical History */}
         <div className="mt-6">
-          <SubmittedFiles files={formData.files} />
+          <SubmittedFiles files={formData?.files} />
         </div>
 
         {/* 4. Billing Info */}
@@ -108,7 +108,7 @@ export default function Confirm() {
             <p><span className="font-medium">Appointment Fee:</span> ৳500</p>
             <p><span className="font-medium">Discount:</span> ৳0</p>
             <p><span className="font-medium">Total Payable:</span> ৳500</p>
-            <p><span className="font-medium">Transaction ID:</span> {formData.transactionId || "Not available"}</p>
+            <p><span className="font-medium">Transaction ID:</span> {formData?.transactionId || "Not available"}</p>
           </div>
         </div>
 
--- app/call/components/CheckPrescription.jsx
+++ app/call/components/CheckPrescription.jsx
@@ -2,18 +2,19 @@ import DoctorShortDetails from "./sub-components/DoctorShortDetails";
 import MetaDetails from "./sub-components/MetaDetails";
 import PatientShortDetails from "./sub-components/PatientShortDetails";
 import { useEffect, useState } from "react";
+import LoadingModal from '@/app/global_components/LoadingModal';
 
 export default function CheckPrescription({ data }) {
   const { prescription, appointment, doctor } = data;
   const [cData, setCData] = useState(null);
   const [loading, setLoading] = useState(false);
 
-   useEffect(() => {
+  useEffect(() => {
     if (!prescription._id) return;
 
     const fetchPrescription = async () => {
       setLoading(true);
-      
+
       try {
         const res = await fetch("/api/prescription/check", {
           method: "POST",
@@ -45,8 +46,8 @@ export default function CheckPrescription({ data }) {
 
 
   console.log("Prescription Data:", cData);
-  
-  
+
+
   return (
     <>
       <div className="flex justify-between mb-4">
@@ -59,6 +60,29 @@ export default function CheckPrescription({ data }) {
       </div>
 
       <PatientShortDetails patient={appointment} />
+
+      <div className="p-4 mt-2 grid gap-4 grid-cols-1 md:grid-cols-2">
+        {cData?.medication.map((med, index) => (
+          <div key={index} className="bg-white rounded-lg p-4 w-full border border-gray-300">
+
+            <h2 className="text-lg font-bold text-purple-700 mb-1">
+              {med.name}
+              <span className="ml-2 text-sm font-medium text-gray-600 italic">
+                ({med.fullData.strength} • {med.fullData.dosageForm})
+              </span>
+            </h2>
+
+            <p className="text-sm text-gray-600 mb-2"><span className="font-bold text-gray-700">Generic:</span> {med.fullData.generic}</p>
+
+            <div className="grid sm:grid-cols-2 grid-cols-1 gap-2 text-sm text-gray-700">
+              <p><span className="font-bold">Frequency:</span> {med.frequency.join(", ")}</p>
+              <p><span className="font-bold">Duration:</span> {med.duration} days</p>
+            </div>
+
+            <p className="text-sm text-gray-700 mt-2"><span className="font-bold">Instruction:</span> {med.instruction}</p>
+          </div>
+        ))}
+      </div>
 
 
 
